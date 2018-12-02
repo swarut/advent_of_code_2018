cc5d9c6c6534149fc5fdc8db552cec6caa02c8ea
Warut Surapat <s.warut@gmail.com>
2018-12-03 01:28:08 +0900

    Rename var.

--- day1/solution.exs
+++ day1/solution.exs
@@ -7,31 +7,31 @@ defmodule Solver do
 
   def get_input do
     {:ok, result} = File.read("input.txt")
-    String.split("\n", trim: true) 
+    String.split(result, "\n", trim: true) 
     |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
-  def part2_solve(acc = %{match: matched, current_freq: current_freq, history: history, time: time}, list) do
+  def part2_solve(acc = %{match: matched, current_freq: current_freq, seen: seen, time: time}, list) do
     IO.puts "TIME: #{inspect(time)}"
     IO.puts "LIST : #{inspect(list)}"
     IO.puts "MATCH : #{inspect(matched)}"
     IO.puts "ACC : #{inspect(acc)}"
-    IO.puts "HISTORY: #{inspect(history)}"
+    IO.puts "SEEN: #{inspect(seen)}"
     IO.puts "---------------------------"
 
     case matched do
       nil -> 
         [h|t] = list
         new_freq = current_freq + h
-        may_be_found = case history[new_freq] do
+        may_be_found = case seen[new_freq] do
           nil -> nil
           _ -> new_freq
         end
-        history = Map.put(history, new_freq, new_freq)
+        seen = Map.put(seen, new_freq, new_freq)
 
         acc = Map.put(acc, :current_freq, new_freq)
         acc = Map.put(acc, :match, may_be_found)
-        acc = Map.put(acc, :history, history)
+        acc = Map.put(acc, :seen, seen)
         acc = Map.put(acc, :time, time + 1 )
         part2_solve(acc, t ++ [h])
       _ -> matched
@@ -41,4 +41,4 @@ defmodule Solver do
 end
 
 IO.puts Solver.part1_solve()
-IO.puts Solver.part2_solve(%{match: nil, current_freq: 0, history: %{}, time: 0}, Solver.get_input())
+IO.puts Solver.part2_solve(%{match: nil, current_freq: 0, seen: %{}, time: 0}, Solver.get_input())
