1edfc7108ddf6434df9baa9ae7d234501eaaffe2
Warut Surapat <s.warut@gmail.com>
2018-12-05 03:59:59 +0900

    Update day 3 part 1.

--- day3/lib/claim.ex
+++ day3/lib/claim.ex
@@ -1,3 +1,3 @@
 defmodule Claim do
-  defstruct  left: 0, top: 0, wide: 0, tall: 0, start_x: 0, end_x: 0, start_y: 0, end_y: 0
+  defstruct  left: 0, top: 0, wide: 0, tall: 0, start_x: 0, end_x: 0, start_y: 0, end_y: 0, id: 0
 end
--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -8,32 +8,35 @@ defmodule Day3 do
   end
 
   def get_spec(line) when is_binary(line) do
-    String.split(line, [" ", "@"], trim:  true)
-    |> Enum.reduce([], fn x, acc ->
-      case String.starts_with?(x, "#") do
-        true -> acc
-        _ -> acc ++ [x]
-      end
-    end)
+    String.split(line, ["#", " @ ", ",",": ", "x"], trim:  true)
+    |> Enum.map(fn x -> String.to_integer(x) end)
   end
 
   def map_claim(spec) when is_list(spec) do
-    [left_top, wide_tall] = spec
-    [left, top] = left_top |> String.split([",", ":"], trim: true) |> Enum.map(fn x -> String.to_integer(x) end)
-    [wide, tall] = wide_tall |> String.split("x") |> Enum.map(fn x -> String.to_integer(x) end)
-    %Claim{left: left, top: top, wide: wide, tall: tall, start_x: left, end_x: left + wide, start_y: top, end_y: top + tall}
-  end
-
-  def compute_overlapped(claim1, claim2) when is_map(claim1) and is_map(claim2) do
-    %Claim{start_x: c1_start_x, end_x: c1_end_x, start_y: c1_start_y, end_y: c1_end_y, wide: c1_wide, tall: c1_tall} = claim1
-    %Claim{start_x: c2_start_x, end_x: c2_end_x, start_y: c2_start_y, end_y: c2_end_y, wide: c2_wide, tall: c2_tall} = claim2
-
+    [id, left, top, wide, tall] = spec
+    %Claim{left: left, top: top, wide: wide, tall: tall, start_x: left, end_x: left + wide, start_y: top, end_y: top + tall, id: id}
   end
 
   def solve do
-    10
-    # claims = get_input("input.txt")
-
+    res = get_input("input.txt")
+    |> Enum.reduce(%{}, fn claim, acc ->
+      Enum.reduce((claim.left + 1)..(claim.left + claim.wide), acc, fn(x, x_acc) ->
+        Enum.reduce((claim.top + 1)..(claim.top + claim.tall), x_acc, fn(y, y_acc) ->
+          case acc[{x, y}] do
+            nil -> Map.put(y_acc, {x, y}, [claim.id])
+            list -> Map.put(y_acc, {x, y}, [claim.id | list])
+          end
+        end)
+      end)
+    end)
+    |> Map.to_list
+    |> Enum.reduce(0, fn {key, value}, acc ->
+      case length(value) do
+        1 -> acc
+        _ -> acc + 1
+      end
+    end)
+    |> inspect
   end
 
 end
