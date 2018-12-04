4f15a26cfb23ae2eb15508ab6233f0adabc6f339
Warut Surapat <wsurapat@gilt.jp>
2018-12-04 20:00:02 +0900

    Progress on day 3.

--- day3/lib/day3.ex
+++ day3/lib/day3.ex
@@ -21,7 +21,19 @@ defmodule Day3 do
     [left_top, wide_tall] = spec
     [left, top] = left_top |> String.split([",", ":"], trim: true) |> Enum.map(fn x -> String.to_integer(x) end)
     [wide, tall] = wide_tall |> String.split("x") |> Enum.map(fn x -> String.to_integer(x) end)
-    %Claim{left: left, top: top, wide: wide, tall: tall}
+    %Claim{left: left, top: top, wide: wide, tall: tall, start_x: left, end_x: left + wide, start_y: top, end_y: top + tall}
+  end
+
+  def compute_overlapped(claim1, claim2) when is_map(claim1) and is_map(claim2) do
+    %Claim{start_x: c1_start_x, end_x: c1_end_x, start_y: c1_start_y, end_y: c1_end_y, wide: c1_wide, tall: c1_tall} = claim1
+    %Claim{start_x: c2_start_x, end_x: c2_end_x, start_y: c2_start_y, end_y: c2_end_y, wide: c2_wide, tall: c2_tall} = claim2
+
+  end
+
+  def solve do
+    10
+    # claims = get_input("input.txt")
+
   end
 
 end
--- day3/test/day3_test.exs
+++ day3/test/day3_test.exs
@@ -7,15 +7,25 @@ defmodule Day3Test do
   end
 
   test "map_claim" do
-    assert Day3.map_claim(["1,3:", "4x4"]) == %Claim{left: 1, top: 3, wide: 4, tall: 4}
+    assert Day3.map_claim(["1,3:", "4x4"]) == %Claim{left: 1, top: 3, wide: 4, tall: 4, start_x: 1, end_x: 5, start_y: 3, end_y: 7}
   end
 
   test "get_input" do
     assert Day3.get_input("input2.txt") == [
-      %Claim{left: 1, top: 3, wide: 4, tall: 4},
-      %Claim{left: 3, top: 1, wide: 4, tall: 4},
-      %Claim{left: 5, top: 5, wide: 2, tall: 2}
+      %Claim{left: 1, top: 3, wide: 4, tall: 4, start_x: 1, end_x: 5, start_y: 3, end_y: 7},
+      %Claim{left: 3, top: 1, wide: 4, tall: 4, start_x: 3, end_x: 7, start_y: 1, end_y: 5},
+      %Claim{left: 5, top: 5, wide: 2, tall: 2, start_x: 5, end_x: 7, start_y: 5, end_y: 7}
     ]
+  end
+
+  test "compute_overlapped" do
+    claim1 = %Claim{left: 1, top: 3, wide: 4, tall: 4, start_x: 1, end_x: 5, start_y: 3, end_y: 7}
+    claim2 = %Claim{left: 3, top: 1, wide: 4, tall: 4, start_x: 3, end_x: 7, start_y: 1, end_y: 5}
+    assert Day3.compute_overlapped(claim1, claim2) == 4
+  end
+
+  test "solve" do
+    assert Day3.solve() == 4
   end
 
 end
