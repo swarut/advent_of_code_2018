f7f860bcb31cc784fcaded6ce8cb40732da9166d
Warut Surapat <wsurapat@gilt.jp>
2018-12-05 19:12:12 +0900

    Finish the parsing part.

--- day4/lib/day4.ex
+++ day4/lib/day4.ex
@@ -2,6 +2,29 @@ defmodule Day4 do
 
   def get_input(filename) do
     {:ok, result} = File.read(filename)
-    String.split(result, "\n", trim: true) |> Enum.map(fn line -> get_spec(line) |> map_claim end)
+    String.split(result, "\n", trim: true)
+  end
+
+  def solve do
+    get_input("input2.txt")
+    |> Enum.reduce(%{current_guard: nil}, fn record, acc ->
+      [year, month, day, h, m] = String.slice(record, 0, 18) |> String.split(["[","-","-"," ",":","]"], trim: true)
+      action_chunk = String.slice(record, 19, String.length(record))
+      c = String.split(action_chunk, " ")
+      IO.puts "C: #{inspect c}"
+      [action, id] = case c do
+        ["Guard", "#" <> id, _, _] -> ["ch-in", id]
+        ["falls", _] -> ["falls", acc[:current_guard]]
+        ["wakes", _] -> ["wakes", acc[:current_guard]]
+      end
+      IO.puts "ACTION: #{action}, ID: #{id}"
+
+      record = %{action: action, timestamp: {year, month, day, h, m}}
+      acc = Map.put(acc, :current_guard, id)
+      |> Map.update(id, [record], fn current_value ->
+        current_value ++ [record]
+      end)
+    end)
+    # |> inspect
   end
 end
--- day4/test/day4_test.exs
+++ day4/test/day4_test.exs
@@ -2,7 +2,24 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  test "parse" do
+    assert Day4.parse(
+      [
+        "[1518-11-01 00:00] Guard #10 begins shift"
+      ]
+    ) == %{year: 1518, month: 11, day: 1, h: "00", m: "00", action: "begin", guard_id: 10}
+  end
+
+  test "parse_date" do
+    assert Day4.parse_date()
+  end
+
+  test "parse_action" do
   end
 end
+
+# Guard #659 begins shift
+# falls asleep
+# wakes up
+# falls asleep
+# falls asleep
