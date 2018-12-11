352cba60b979568465e763fd3fc4581990a74600
Warut Surapat <wsurapat@gilt.jp>
2018-12-11 19:06:25 +0900

    Start day 10 just for fun with office colleague.

---
+++ day10/lib/day10.ex
@@ -0,0 +1,68 @@
+defmodule Day10 do
+  @moduledoc """
+  Documentation for Day10.
+  """
+
+  @doc """
+  Get input
+
+  ## Examples
+
+      iex> Day10.get_input("input2.txt")
+      [
+        "position=\< 9,  1\> velocity=\< 0,  2\>",
+        "position=\< 7,  0\> velocity=\<-1,  0\>",
+        "position=\< 3, -2\> velocity=\<-1,  1\>",
+        "position=\< 6, 10\> velocity=\<-2, -1\>",
+        "position=\< 2, -4\> velocity=\< 2,  2\>",
+        "position=\<-6, 10\> velocity=\< 2, -2\>",
+        "position=\< 1,  8\> velocity=\< 1, -1\>",
+        "position=\< 1,  7\> velocity=\< 1,  0\>",
+        "position=\<-3, 11\> velocity=\< 1, -2\>",
+        "position=\< 7,  6\> velocity=\<-1, -1\>",
+        "position=\<-2,  3\> velocity=\< 1,  0\>",
+        "position=\<-4,  3\> velocity=\< 2,  0\>",
+        "position=\<10, -3\> velocity=\<-1,  1\>",
+        "position=\< 5, 11\> velocity=\< 1, -2\>",
+        "position=\< 4,  7\> velocity=\< 0, -1\>",
+        "position=\< 8, -2\> velocity=\< 0,  1\>",
+        "position=\<15,  0\> velocity=\<-2,  0\>",
+        "position=\< 1,  6\> velocity=\< 1,  0\>",
+        "position=\< 8,  9\> velocity=\< 0, -1\>",
+        "position=\< 3,  3\> velocity=\<-1,  1\>",
+        "position=\< 0,  5\> velocity=\< 0, -1\>",
+        "position=\<-2,  2\> velocity=\< 2,  0\>",
+        "position=\< 5, -2\> velocity=\< 1,  2\>",
+        "position=\< 1,  4\> velocity=\< 2,  1\>",
+        "position=\<-2,  7\> velocity=\< 2, -2\>",
+        "position=\< 3,  6\> velocity=\<-1, -1\>",
+        "position=\< 5,  0\> velocity=\< 1,  0\>",
+        "position=\<-6,  0\> velocity=\< 2,  0\>",
+        "position=\< 5,  9\> velocity=\< 1, -2\>",
+        "position=\<14,  7\> velocity=\<-2,  0\>",
+        "position=\<-3,  6\> velocity=\< 2, -1\>"
+      ]
+  """
+  def get_input(filename) do
+    {:ok, result} = File.read(filename)
+    String.split(result, "\n", trim: true)
+  end
+
+  @doc """
+  Parse input
+
+  ## Example
+      iex> Day10.parse_input(["position=\<-3,  6\> velocity=\< 2, -1\>"])
+      [%{x: -3, y: 6, vx: 2, vy: -1}]
+
+      iex> Day10.parse_input(["position=\< 5,  9\> velocity=\< 1, -2\>",])
+      [%{x: 5, y: 9, vx: 1, vy: -2}]
+  """
+  def parse_input(list) do
+    list
+    |> Enum.map(fn item ->
+      [x, y, vx, vy] = String.replace(item, ~r{\s}, "") |> String.split(["position=<", ",",  ">velocity=<", ",", ">"], trim: true)
+      %{x: String.to_integer(x), y: String.to_integer(y), vx: String.to_integer(vx), vy: String.to_integer(vy)}
+    end)
+  end
+end
