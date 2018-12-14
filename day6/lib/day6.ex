56ecc488c6f3bbe81c5bcc85dbdfe0e26a4941b3
swarut <s.warut@gmail.com>
2018-12-14 11:01:02 +0900

    Prepare for day 6.

--- day6/lib/day6.ex
+++ day6/lib/day6.ex
@@ -1,18 +1,3 @@
 defmodule Day6 do
-  @moduledoc """
-  Documentation for Day6.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Day6.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- day6/test/day6_test.exs
+++ day6/test/day6_test.exs
@@ -1,8 +1,4 @@
 defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
-
-  test "greets the world" do
-    assert Day6.hello() == :world
-  end
 end
