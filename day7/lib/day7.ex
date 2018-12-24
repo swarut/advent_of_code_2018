a9cb2f48c82d44956d4142ef35e7cb7474852648
Warut Surapat <s.warut@gmail.com>
2018-12-24 23:36:39 +0900

    Init day 7.

---
+++ day7/lib/day7.ex
@@ -0,0 +1,18 @@
+defmodule Day7 do
+  @moduledoc """
+  Documentation for Day7.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Day7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
