1446a00ed9dccb811cab037415ca49ebb6a402d9
Warut Surapat <s.warut@gmail.com>
2018-12-25 02:38:01 +0900

    Add some basic start for day 7.

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -1,18 +1,27 @@
 defmodule Day7 do
-  @moduledoc """
-  Documentation for Day7.
-  """
+
+  def get_input(filename) do
+    {:ok, result} = File.read(filename)
+    String.split(result, "\n", trim: true)
+    |> Enum.map(fn co ->
+      [x, y] = co |> String.split(["Step ", " must be finished before step ", " can begin."], trim: true)
+      {x, y}
+    end)
+  end
 
   @doc """
-  Hello world.
+  Find start point and end point
 
-  ## Examples
-
-      iex> Day7.hello()
-      :world
-
+  ## Example
+      iex> Day7.find_start_and_end_points([{"A", "B"}, {"A", "C"}, {"B", "D"}, {"C", "D"}])
+      {["A"], ["D"]}
   """
-  def hello do
-    :world
+  def find_start_and_end_points(tuples) do
+    {elements_at_first_position, elements_at_second_position} = tuples
+    |> Enum.reduce({[], []}, fn({a, b}, {a_col, b_col}) ->
+      {([a] ++ a_col) |> Enum.uniq, ([b] ++ b_col) |> Enum.uniq}
+    end)
+    {elements_at_first_position -- elements_at_second_position, elements_at_second_position -- elements_at_first_position}
   end
+
 end
