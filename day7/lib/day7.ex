db9f24ca073d5f05c146aff26dc251b6e5e734cf
swarut <s.warut@gmail.com>
2018-12-26 18:43:08 +0900

    Progress on day 7.

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -38,23 +38,36 @@ defmodule Day7 do
   """
   def proceed(data) do
     # figure out the fisrt element
-    c = first_element(data)
-    proceed(data, c, "")
+    {a, b} = first_element(data)
+    proceed(data, b, [a])
   end
-  def proceed([], cursor, acc) do
 
+  def proceed([], cursor, acc) do
+    finalize_output(acc)
+  end
+
+  def proceed(data, cursor, acc) do
+    []
+    next = data |> Enum.find(fn {a, b} -> a == cursor end)
+    case next do
+      nil -> nil
+      _ ->
+        {x, y} = next
+        acc = [x] ++ acc
+        cursor = acc
+        proceed(List.delete(data, {a, b}), cursor, acc)
+    end
   end
 
   @doc """
   Finalize the output
 
   ## Example
-      iex> Day7.finalize_output("EFEDEBAC")
+      iex> Day7.finalize_output(["E", "F", "E", "D", "E", "B", "A", "C"])
       "CABDFE"
   """
-  def finalize_output(str) do
-    (str
-     |> String.graphemes()
+  def finalize_output(char_list) do
+    (char_list
      |> Enum.reduce(%{result: "", lookup: MapSet.new()}, fn c, acc ->
        case MapSet.member?(acc.lookup, c) do
          true ->
@@ -66,3 +79,58 @@ defmodule Day7 do
      end)).result
   end
 end
+
+
+# {"C", "A"}	o
+# {"C", "F"}	o
+# {"A", "B"}	o
+# {"A", "D"}	o
+# {"B", "E"}	o
+# {"D", "E"}	o
+# {"F", "E"}
+
+
+
+# C A {"C", "A"}      		cursor A
+
+# C A B {"A", "B"}    		cursor B
+
+# C A B E {"B", "E"}		cursor E
+
+# no E
+
+# move cursor bck			cursor B
+
+# no B
+
+# move cusor back			cussor A
+
+# C A B E D {"A", "D"}		cursor D
+
+# C A B E D E	{"D", "E"}	cursor E
+
+# no e
+
+# move cursor back			cursor D
+
+# no D
+
+# move curso back			cursor E
+
+# no E
+
+# move cursor back			cursor B
+
+# no B
+
+# move cursor back    		cursor A
+
+# no A
+
+# move cursor back    		cursor C
+
+# C A B E D E F	{"C", "F"}
+
+# C A B E D E F E {"F", "E"}
+
+# no input
