7cb54042dbc684bd3e4b458621d762e6d6d6d633
swarut <s.warut@gmail.com>
2018-12-27 18:41:58 +0900

    Restart day 7.

--- day7/lib/day7.ex
+++ day7/lib/day7.ex
@@ -30,80 +30,64 @@ defmodule Day7 do
      (elements_at_second_position -- elements_at_first_position) |> Enum.reverse}
   end
 
+  # @doc """
+  # Process the given tuples
+
+  # ## Example
+  #     iex> Day7.proceed([{"A", "B"}, {"A", "C"}, {"B", "D"}, {"C", "D"}])
+  #     "ABCD"
+  #     iex> Day7.proceed([{"A", "B"}, {"B", "C"}, {"D", "B"}])
+  #     "ADBC"
+  #     iex> Day7.proceed([{"M", "D"}, {"X", "Y"}, {"Y", "D"}])
+  #     "MXYD"
+  #     iex> Day7.proceed([{"A", "B"},{"B", "C"},{"B", "E"},{"C", "Z"},{"D", "B"},{"E", "Z"}])
+  #     "ADBCEZ"
+  #     iex> Day7.proceed([{"A", "B"},{"A", "C"},{"A", "D"},{"B", "E"},{"B", "X"},{"C", "E"},{"D", "E"},{"E", "Z"},{"X", "Z"}])
+  #     "ABCDEXZ"
+  #     iex> Day7.proceed([{"A", "B"},{"A", "C"},{"A", "X"},{"B", "D"},{"B", "E"},{"C", "E"},{"D", "Z"},{"E", "Z"},{"X", "E"}])
+  #     "ABCDXEZ"
+  # """
+  # def proceed(tuples) do
+  #   # figure out the fisrt element
+  #   {[first | _], _} = find_start_and_end_points(tuples)
+  #   proceed(tuples, [first], [first])
+  # end
+
   @doc """
-  Process the given tuples
+  Generate incoming lookup
 
   ## Example
-      iex> Day7.proceed([{"A", "B"}, {"A", "C"}, {"B", "D"}, {"C", "D"}])
-      "ABCD"
-      iex> Day7.proceed([{"A", "B"}, {"B", "C"}, {"D", "B"}])
-      "ADBC"
-      iex> Day7.proceed([{"M", "D"}, {"X", "Y"}, {"Y", "D"}])
-      "MXYD"
-      iex> Day7.proceed([{"A", "B"},{"B", "C"},{"B", "E"},{"C", "Z"},{"D", "B"},{"E", "Z"}])
-      "ADBCEZ"
-      iex> Day7.proceed([{"A", "B"},{"A", "C"},{"A", "D"},{"B", "E"},{"B", "X"},{"C", "E"},{"D", "E"},{"E", "Z"},{"X", "Z"}])
-      "ABCDEXZ"
-      iex> Day7.proceed([{"A", "B"},{"A", "C"},{"A", "X"},{"B", "D"},{"B", "E"},{"C", "E"},{"D", "Z"},{"E", "Z"},{"X", "E"}])
-      "ABCDXEZ"
+      iex> Day7.incoming_lookup([{"A", "B"}, {"B", "C"}])
+      %{
+        "B" => ["A"],
+        "C" => ["B"]
+      }
   """
-  def proceed(tuples) do
-    # figure out the fisrt element
-    {[first | _], _} = find_start_and_end_points(tuples)
-    proceed(tuples, [first], [first])
+  def incoming_lookup(tuples) do
+    tuples
+    |> Enum.reduce(%{}, fn {a, b}, acc ->
+      Map.update(acc, b, [a], fn current_value ->
+        [a] ++ current_value
+      end)
+    end)
   end
 
-  def proceed([], cursor, acc) do
-    IO.puts(" PROCEED tuples = [], cursor = #{inspect cursor}, acc = #{inspect acc, limit: :infinity}")
-    finalize_output(acc)
-  end
+  @doc """
+  Generate outgoing lookup
 
-  def proceed(tuples, [head_of_cursor | rest_of_cursor] = cursor, acc) do
-    IO.puts(" PROCEED tuples = #{inspect tuples}, cursor = #{inspect cursor}, acc = #{inspect acc, limit: :infinity}")
-
-    tuple_ended_with_head_of_cursor = tuples |> Enum.find(fn {a, b} -> b == head_of_cursor end)
-    case tuple_ended_with_head_of_cursor do
-      nil ->
-        IO.puts("---- item end with #{head_of_cursor} is not found")
-        tuple_starting_with_head_of_cursor = tuples |> Enum.find(fn {a, b} -> a == head_of_cursor end)
-        case tuple_starting_with_head_of_cursor do
-          nil ->
-            IO.puts("---- item begin with #{head_of_cursor} is not found")
-            proceed(tuples, rest_of_cursor, acc)
-          _ ->
-            IO.puts("---- found item begin with #{head_of_cursor} = #{inspect tuple_starting_with_head_of_cursor}")
-            {x, y} = tuple_starting_with_head_of_cursor
-            acc = [y] ++ acc
-            cursor = acc
-            proceed(List.delete(tuples, tuple_starting_with_head_of_cursor), cursor, acc)
-        end
-      _ ->
-        IO.puts("---- found item end with #{head_of_cursor} = #{inspect tuple_ended_with_head_of_cursor}")
-
-        replacement_index = Enum.find_index(acc, fn x -> x == head_of_cursor end)
-        {x, y} = tuple_ended_with_head_of_cursor
-        acc = acc |> List.replace_at(replacement_index, [y, x]) |> List.flatten
-        cursor = acc
-        proceed(List.delete(tuples, tuple_ended_with_head_of_cursor), cursor, acc)
-    end
-  end
-
-  def proceed(tuples, [] = cursor, acc) do
-    {[first | _], _} = find_start_and_end_points(tuples)
-    proceed(tuples, [first], [first] ++ acc)
-  end
-
-  def finalize_output(char_list) do
-    (char_list
-      |> Enum.reverse
-      |> Enum.reduce(%{result: "", lookup: MapSet.new()}, fn c, acc ->
-       case MapSet.member?(acc.lookup, c) do
-         true ->
-           acc
-
-         false ->
-           Map.put(acc, :result, acc.result <> c) |> Map.put(:lookup, MapSet.put(acc.lookup, c))
-       end
-     end)).result
+  ## EXample
+      iex> Day7.outgoing_lookup([{"A", "B"}, {"B", "C"}])
+      %{
+        "A" => ["B"],
+        "B" => ["C"]
+      }
+  """
+  def outgoing_lookup(tuples) do
+    tuples
+    |> Enum.reduce(%{}, fn {a, b}, acc ->
+      Map.update(acc, a, [b], fn current_value ->
+        [b] ++ current_value
+      end)
+    end)
   end
 end
